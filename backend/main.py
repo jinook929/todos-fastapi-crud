8c24a4a0ef8078576628daf57670c821c1152506
Jinook Jung <jinookjung@Jinooks-MacBook-Pro.local>
2025-01-01 13:46:07 -0500

    update origins

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ origins = [
     "http://localhost:4173",
     "http://localhost:3030",
     "https://jinookjung.dev",
-    "http://jncweb.icu"
+    "http://216.250.124.30"
 ]
 
 app.add_middleware(
